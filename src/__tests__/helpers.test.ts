b8a675a26b0fe5bce5bfc404d7bb8d004d9c16a3
valentin Mariot <valentinmariot@gmail.com>
2022-12-11 19:34:22 +0100

    [IMPROVE]: formatDate() and test

---
+++ src/__tests__/helpers.test.ts
@@ -0,0 +1,8 @@
+import { formatDate } from "@/utils/helpers";
+
+describe("formatDate", () => {
+  it("should return the date in the format YYYY-MM-DD", () => {
+    const date = new Date("2022-12-09");
+    expect(formatDate(date)).toBe("2022-12-09");
+  });
+});
