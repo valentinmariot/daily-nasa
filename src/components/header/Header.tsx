1c78bc99903e5f5f039b1c98c1f995947778682b
valentin Mariot <valentinmariot@gmail.com>
2022-12-11 22:52:05 +0100

    [INIT]: header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import "./Header.scss";
+
+const Header: FC = () => {
+  return (
+    <div className="header">
+      <div className="header_title">
+        <h1>DAILY NASA 🚀</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
